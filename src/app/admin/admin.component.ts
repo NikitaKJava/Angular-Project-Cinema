dc4a2f1366e8ad4d268df06d47b1284efbee7988
NiKo <80517411+NikitaKJava@users.noreply.github.com>
2023-01-12 20:06:18 +0100

    new commitment

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,5 +1,7 @@
 import {Component, Input} from '@angular/core';
 import {IMovie} from "../models/movie";
+import { AfterViewInit, ElementRef} from '@angular/core';
+
 
 @Component({
   selector: 'app-admin',
@@ -9,5 +11,19 @@ import {IMovie} from "../models/movie";
 
 
 export class AdminComponent {
+
+  seat_rows: number | null | undefined;
+  seat_columns: number;
+
   @Input() movies: IMovie;
+  private myInput: any;
+
+  constructor(private elementRef:ElementRef) {}
+
+
+  ngAfterViewInit() {
+    this.myInput.nativeElement.addEventListener('click', () => {
+      console.log('Button clicked');
+    });
+  }
 }
