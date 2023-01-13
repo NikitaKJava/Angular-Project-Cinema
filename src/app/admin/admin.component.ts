10884aae8ae7969412827969a339a3533e6fe6de
mattkush <121498353+mattkush@users.noreply.github.com>
2023-01-13 18:07:35 +0100

    Change for seat selection

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -11,15 +11,16 @@ import {IMovie} from "../models/movie";
 
 
 export class AdminComponent {
-  rowArray = 1
-  colArray = 1
-  seat_rows: number[] = [this.rowArray]
-  seat_columns: number[] = [this.colArray]
+  disabled: number[] = new Array();
+  deluxe: number[] = new Array();
 
   // @ViewChild('rowsInput') sr: number = 0
   // @ViewChild('columnsInput') sc: ElementRef
   @Input() movies: IMovie;
   @ViewChild('cinemaSeats') cinemaSeats: ElementRef;
+  @ViewChild('normalSeatSelector') normalSeatSelector: ElementRef;
+  @ViewChild('deluxeSeatSelector') deluxeSeatSelector: ElementRef;
+  @ViewChild('disabledSeatSelector') disabledSeatSelector: ElementRef;
 
   constructor(private elementRef: ElementRef, private injector: Injector, private componentFactoryResolver: ComponentFactoryResolver) {
   }
@@ -35,14 +36,6 @@ export class AdminComponent {
   }
 
   onCreateClick(rows: string, columns: string) {
-    this.rowArray = Number(rows);
-    this.colArray = Number(columns);
-    // this.sr.nativeElement.addEventListener('click', () => {
-    //   console.log('Button clicked');
-    // });
-
-    console.log(this.seat_rows, " ", this.seat_columns);
-    console.log(parseInt(rows) + " ", parseInt(columns));
     for (let i = 0; i < parseInt(rows); i++) {
       let row = document.createElement('div');
       for (let j = 1; j <= parseInt(columns); j++) {
@@ -54,24 +47,37 @@ export class AdminComponent {
       }
       this.cinemaSeats.nativeElement.appendChild(row);
     }
-    // const factory = this.componentFactoryResolver.resolveComponentFactory(AdminComponent);
-    // const componentRef = factory.create(this.injector);
-    // componentRef.changeDetectorRef.detectChanges();
-    // console.log(this.seat_rows, " ", this.seat_columns)
-    // console.log(this.rowArray, " ", this.colArray)
   }
 
   Number(value: string) {
     return parseInt(value);
   }
 
-  onSeatClick(event: MouseEvent) {
+  onSeatClick(event: Event) {
     console.log('Seat clicked');
-    console.log('event element', event);
-    if ((event.target as HTMLElement).tagName === 'A') {
-      let target = event.target as HTMLElement;
-      alert(target.innerHTML)
+    const target = event.target as HTMLElement;
+    let num = parseInt(target.innerHTML);
+    
+    if(this.normalSeatSelector.nativeElement.checked === true){
+      if(this.disabled.includes(num)){
+        this.disabled.splice(this.disabled.indexOf(num),1);
+      }
+      if(this.deluxe.includes(num)){
+        this.deluxe.splice(this.deluxe.indexOf(num),1);
+      }
+    }else if(this.deluxeSeatSelector.nativeElement.checked === true){
+      if(this.disabled.includes(num)){
+        this.disabled.splice(this.disabled.indexOf(num),1);
+      }
+      this.deluxe.push(num);
+    }else if(this.disabledSeatSelector.nativeElement.checked === true){
+      if(this.deluxe.includes(num)){
+        this.deluxe.splice(this.deluxe.indexOf(num),1);
+      }
+      this.disabled.push(num);
     }
+    console.log(this.disabled);
+    console.log(this.disabled);
     // Perform any desired action
   }
 }
