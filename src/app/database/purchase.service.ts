443e6617883d02b505d2890bdf1ccf3f254809de
mattkush <121498353+mattkush@users.noreply.github.com>
2023-02-11 20:42:33 +0100

    modifications to make everything compatible

--- src/app/database/purchase.service.ts
+++ src/app/database/purchase.service.ts
@@ -5,6 +5,7 @@ import {Injectable} from "@angular/core"; // interface
 
 import {IShow, Show} from "../models/show";
 import {IPurchase, Purchase} from "../models/purchase"; // interface, class
+import { ITicket } from "../models/ticket";
 
 const httpOptions = {
   headers: new HttpHeaders({
@@ -22,11 +23,11 @@ export class PurchaseService {
   constructor(private messageService: MessageService, private http: HttpClient) {}
 
   /** POST: add a new purchase to the database */
-  addPurchase(purchases: IPurchase[]): Observable<any> {
+  addPurchase(purchases: Purchase[]): Observable<any> {
     this.messageService.add('PurchaseService: add show');
     const body = JSON.stringify(purchases);
     console.log(body)
-    return this.http.post<Purchase[]>('http://localhost:3000/api/ticketing/buyTicket', body, httpOptions)
+    return this.http.post<Purchase[]>('http://localhost:3000/api/ticketing/buyticket', body, httpOptions)
       .pipe(
         catchError(err => {
           return ("ADD PURCHASE ERROR: " + err);
@@ -35,23 +36,25 @@ export class PurchaseService {
   }
 
   /** GET: get a specific purchase by show ID from the database */
-  getPurchasesByShowID(id: string): Observable<IPurchase[]> {
+  getPurchasesByShowID(id: number): Observable<ITicket[]> {
     this.messageService.add('PurchaseService: shows fetched');
-    return this.http.get<IPurchase[]>('http://localhost:3000/api/show/' + id + '/purchases');
+    return this.http.get<ITicket[]>('http://localhost:3000/api/ticketing/ticketsinfo/show/' + id);
+  }
+ /** GET ALL: get a specific purchase by show ID from the database */
+  getAllPurchasesByShowID(id: number): Observable<ITicket[]> {
+    this.messageService.add('PurchaseService: shows fetched');
+    return this.http.get<ITicket[]>('http://localhost:3000/api/ticketing/allticketsinfo/show/' + id);
+  }
+    /** GET: get a specific purchase by show ID from the database */
+  getPurchasesByTicketID(id: number): Observable<ITicket[]> {
+    this.messageService.add('PurchaseService: shows fetched');
+    return this.http.get<ITicket[]>('http://localhost:3000/api/ticketing/ticketsinfo/' + id);
   }
 
-  /** GET: get all shows from the database */
-  getShows(): Observable<IPurchase[]> {
-    this.messageService.add('PurchaseService: shows fetched');
-    return this.http.get<IPurchase[]>('http://localhost:3000/api/show/');
-  }
-
-  /** DELETE: delete a selected show by ID from database */
-  deleteShow(id: number): Observable<any> {
-    this.messageService.add('PurchaseService: delete show');
-    const body = JSON.stringify(id);
-    console.log(body)
-    return this.http.post<IPurchase>('http://localhost:3000/api/show/' + id + '/purchase/id', body, httpOptions)
+  /** DELETE: delete a selected ticket by ID from database */
+  deleteTicket(id: number): Observable<any> {
+    this.messageService.add('PurchaseService: delete ticket');
+    return this.http.delete<IPurchase>('http://localhost:3000/api/ticketing/ticket/' + id, httpOptions)
       .pipe(
         catchError(async () => console.log("DELETE PURCHASE ERROR"))
       );
